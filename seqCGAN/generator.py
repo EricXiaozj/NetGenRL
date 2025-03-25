fix bug of softmax

seqCGAN/generator.py
@@ -33,9 +33,9 @@ class Generator(nn.Module):
         #     nn.ReLU(True),
         # )
         
-        self.emb_now = nn.ModuleList([
-            nn.Embedding(x_len, self.embedding_dim) for x_len in x_list[:-1]
-        ]) 
+        # self.emb_now = nn.ModuleList([
+        #     nn.Embedding(x_len, self.embedding_dim) for x_len in x_list[:-1]
+        # ]) 
         
         self.pad_embedding = nn.Parameter(torch.zeros(1, self.attribute_dim))
         
@@ -72,6 +72,17 @@ class Generator(nn.Module):
                 nn.Identity()
             ) for x_len in x_list
         ])
+        # self.output_layer = nn.ModuleList([
+        #     nn.Sequential(
+        #         nn.Linear(self.hidden_dim, x_len),
+        #         nn.Identity()
+        #     ) for x_len in x_list
+        # ])
+        # self.output_layer = nn.Sequential(
+        #     nn.Linear(self.hidden_dim, self.pred_dim),
+        #     nn.Identity()
+        # )
+
         
         self.softmax = nn.LogSoftmax(dim=-1)
         
@@ -93,7 +104,7 @@ class Generator(nn.Module):
         # emb_list = torch.stack([torch.cat([self.emb[idx][j](x[i,:,j]) for j in range(len(self.x_list))], dim=1) for i, idx in enumerate(label_int)],dim=0)
         emb_list = torch.cat([self.emb[i](x[:,:,i]) for i in range(len(self.x_list))], dim=2)
         
-        now_emb_list = torch.cat([self.emb_now[i](x_now[:,:,i]) for i in range(len(self.x_list) - 1)], dim=2)
+        now_emb_list = torch.cat([self.emb[i](x_now[:,:,i]) for i in range(len(self.x_list)-1)], dim=2)
         attribute_features = [self.condition_fix[i](now_emb_list[:,:,:(i+1)*self.embedding_dim]) for i in range(len(self.x_list) - 1)]
         # attribute_features = [torch.zeros(x_now.shape[0],x_now.shape[1],self.attribute_dim).to(self.device)] + attribute_features
         attribute_features = [self.pad_embedding.expand(x_now.shape[0],x_now.shape[1],self.attribute_dim)] + attribute_features
@@ -127,12 +138,59 @@ class Generator(nn.Module):
             hidden_w_attr = torch.cat([hidden_output, attribute_features[i]], dim=2)
             output = self.output_layer[i](hidden_w_attr.contiguous().view(-1, self.hidden_dim + self.attribute_dim))
             # pred = self.softmax(output[:,count:count + x_len])
-            pred = F.softmax(output,dim=-1)
+            # output = self.output_layer[i](hidden_output.contiguous().view(-1, self.hidden_dim))
+            pred = self.softmax(output)
             preds.append(pred)
             # count += x_len
+        
+        # count = 0  
+        # for x_len in self.x_list:
+        #     output = self.output_layer(hidden_output.contiguous().view(-1, self.hidden_dim))
+        #     pred = self.softmax(output[:,count:count + x_len])
+        #     preds.append(pred)
+        #     count += x_len
             
         preds = torch.cat(preds, dim=1).view(-1, self.max_seq_len, self.pred_dim) 
         return preds
+    
+    def get_LSTM_hidden(self, label, length, x, h, c):
+        
+        length_one_hot = F.one_hot((length.clone().detach().long() - 1), num_classes=self.max_seq_len).float().to(self.device)
+        
+        length_out = self.length_fc(length_one_hot)
+        label_int = torch.argmax(label.clone(),1)
+        
+        emb_list = torch.cat([self.emb[i](x[:,i]) for i in range(len(self.x_list))], dim=1)
+        
+        combined = torch.cat([emb_list, length_out], dim=1)
+
+        fc_outputs = torch.stack([self.combine_fc[idx](combined) for idx in range(len(self.combine_fc))], dim=1)
+        
+        indices = label_int.view(-1, 1, 1).expand(-1, -1, fc_outputs.size(2))
+
+        combined_out = torch.gather(fc_outputs, dim=1, index=indices)
+
+        hidden_output, (h, c) = self.lstm(combined_out, (h, c))
+        
+        hidden_output = hidden_output.squeeze(1)        
+            
+        return hidden_output, h, c
+    
+    def step_w_hidden(self, hidden_output, x_now, dim_now):
+        if dim_now > 0 and x_now is not None:
+            now_emb_list = torch.cat([self.emb[i](x_now[:,i]) for i in range(dim_now)], dim=1)
+            attribute_feature = self.condition_fix[dim_now - 1](now_emb_list)
+        else:
+            # attribute_feature = torch.zeros(x.shape[0],self.attribute_dim).to(self.device)
+            attribute_feature = self.pad_embedding.expand(hidden_output.shape[0],self.attribute_dim)
+            
+        hidden_w_attr = torch.cat([hidden_output, attribute_feature], dim=1)
+        output = self.output_layer[dim_now](hidden_w_attr.contiguous())
+        # output = self.output_layer[dim_now](hidden_output.view(-1, self.hidden_dim))
+        # output = self.output_layer(hidden_output.view(-1, self.hidden_dim))
+        # count = sum(self.x_list[:dim_now])
+        pred = F.softmax(output,dim=-1)
+        return pred
     
     def step(self, label, length, x, x_now, dim_now, h, c):
         """
@@ -154,7 +212,7 @@ class Generator(nn.Module):
         emb_list = torch.cat([self.emb[i](x[:,i]) for i in range(len(self.x_list))], dim=1)
         
         if dim_now > 0 and x_now is not None:
-            now_emb_list = torch.cat([self.emb_now[i](x_now[:,i]) for i in range(dim_now)], dim=1)
+            now_emb_list = torch.cat([self.emb[i](x_now[:,i]) for i in range(dim_now)], dim=1)
             attribute_feature = self.condition_fix[dim_now - 1](now_emb_list)
         else:
             # attribute_feature = torch.zeros(x.shape[0],self.attribute_dim).to(self.device)
@@ -175,8 +233,9 @@ class Generator(nn.Module):
         
         hidden_output = hidden_output.squeeze(1)
         # print(hidden_output.shape, attribute_feature.shape)
-        hidden_w_attr = torch.cat([hidden_output, attribute_feature], dim=1)
-        output = self.output_layer[dim_now](hidden_w_attr.contiguous())
+        # hidden_w_attr = torch.cat([hidden_output, attribute_feature], dim=1)
+        # output = self.output_layer[dim_now](hidden_w_attr.contiguous())
+        output = self.output_layer[dim_now](hidden_output.view(-1, self.hidden_dim))
         pred = F.softmax(output,dim=-1)
         
         # output = self.output_layer(hidden_output.view(-1, self.hidden_dim))
@@ -208,13 +267,15 @@ class Generator(nn.Module):
             for i in range(self.max_seq_len): 
                 last = None
                 sam = []
+                hidden_output, h, c = self.get_LSTM_hidden(label, length, x, h, c)
                 for j in range(len(self.x_list)):
-                    pred, h_new, c_new = self.step(label, length, x, last, j, h, c)
+                    # pred, h_new, c_new = self.step(label, length, x, last, j, h, c)
+                    pred = self.step_w_hidden(hidden_output, last, j)
                     sam.append(pred.multinomial(1))
                     last = torch.cat(sam,dim=1)
                 x = last
-                h = h_new
-                c = c_new
+                # h = h_new
+                # c = c_new
                 samples.append(x)
         else:
             # print(x.shape)
@@ -223,16 +284,22 @@ class Generator(nn.Module):
             sam = []
             for i in range(given_len):
                 input = lis[i].squeeze(1)
-                for j in range(len(self.x_list)):
-                    if j == 0:
-                        pred, h_new, c_new = self.step(label, length, input, None, j, h, c)
-                    else:
-                        pred, h_new, c_new = self.step(label, length, input, input[:,:j], j, h, c)
-                    if i == given_len - 1:
+                # for j in range(len(self.x_list)):
+                #     if j == 0:
+                # pred, h_new, c_new = self.step(label, length, input, None, 0, h, c)
+                hidden_output, h, c = self.get_LSTM_hidden(label, length, input, h, c)
+                    # else:
+                    #     pred, h_new, c_new = self.step(label, length, input, input[:,:j], j, h, c)
+                if i == given_len - 1:
+                    pred = self.step_w_hidden(hidden_output, None, 0)
+                    sam.append(pred.multinomial(1))
+                    for j in range(1,len(self.x_list)):
+                        pred = self.step_w_hidden(hidden_output, input[:,:j], j)
+                        # pred, h_new, c_new = self.step(label, length, input, input[:,:j], j, h, c)
                         sam.append(pred.multinomial(1))
                 samples.append(input)
-                h = h_new
-                c = c_new
+                # h = h_new
+                # c = c_new
   
             sam_tensor = torch.cat(sam,dim=1)
             # print(sam_tensor.shape)
@@ -241,13 +308,15 @@ class Generator(nn.Module):
                 samples.append(sam_tensor)
                 sam = []
                 last = None
+                hidden_output, h, c = self.get_LSTM_hidden(label, length, sam_tensor, h, c)
                 for j in range(len(self.x_list)):
-                    pred, h_new, c_new = self.step(label, length, sam_tensor, last, j, h, c)
+                    pred = self.step_w_hidden(hidden_output, last, j)
+                    #pred, h_new, c_new = self.step(label, length, sam_tensor, last, j, h, c)
                     sam.append(pred.multinomial(1))
                     last = torch.cat(sam,dim=1)
                 sam_tensor = last
-                h = h_new
-                c = c_new
+                # h = h_new
+                # c = c_new
         output = torch.stack(samples, dim=1)
         # print(output.shape)
         return output

seqCGAN/main.py
@@ -104,7 +104,8 @@ def pre_train(generator, discriminator, dataloader, generator_epoch, discirminat
             
             fake_preds = generator.forward(labels, lengths, seqs_seed, seqs) # (batch_size, seq_len, prob_dim)
             
-            target = torch.cat([seqs[:,:-1,:], zero], dim=1) # (batch_size, seq_len, seq_dim)
+            # target = torch.cat([seqs[:,:-1,:], zero], dim=1) # (batch_size, seq_len, seq_dim)
+            target = seqs # (batch_size, seq_len, seq_dim)
             
             count = 0
             